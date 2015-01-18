8977851baa88bb0f1081dae254b275284b922eca
yojan shrestha <sickgrin@gmail.com>
2015-01-17 19:16:49 -0600

    Getting manifest file from github done.
    - manifest file (index.js) is now pulled from github
    - exec command - node index.js create username/reponame

--- commands/create.js
+++ commands/create.js
@@ -1,9 +1,14 @@
 'use strict';
 
+var download = require('../lib/download');
+// var mkdirs = require('mkdirs');
+
 module.exports = {
   "command": "create <manifest>",
   "description": "Creates the scaffolding based on the provided manifest file",
   "action": function(manifest) {
-    console.log(manifest);
+		var test = download(manifest, function(yo){
+			console.log(yo);
+		});
   }
 }
