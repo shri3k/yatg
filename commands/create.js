b5b08e5fb91eb0e010a531d9678f3b45958bf530
yojan shrestha <sickgrin@gmail.com>
2015-01-17 22:07:31 -0600

    Directory strucuture done
    - mkdirs can now create directories

--- commands/create.js
+++ commands/create.js
@@ -4,11 +4,18 @@ var download = require('../lib/download');
 // var mkdirs = require('mkdirs');
 
 module.exports = {
-  "command": "create <manifest>",
+  "command": "create <manifest> <projectName>",
   "description": "Creates the scaffolding based on the provided manifest file",
-  "action": function(manifest) {
-		var test = download(manifest, function(yo){
-			console.log(yo);
-		});
+  "action": function(manifest, projectName) {
+    var test = download(manifest, function(yo) {
+      var manifestContent = null;
+      console.log(yo);
+      try {
+        manifestContent = JSON.parse(yo);
+      } catch (e) {
+        console.log(e);
+      }
+      require('../lib/mkdirs')(projectName, Object.keys(manifestContent));
+    });
   }
 }
