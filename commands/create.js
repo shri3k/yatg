b71ca0310c4cd1ccd50877a2e149fc9cbab5c347
yojan shrestha <sickgrin@gmail.com>
2015-01-19 01:25:12 -0600

    v0.1.0 done
    - User can now create a template
    - Template manifest file is pulled from the github
    - Project template is created based on the manifest file
    - Appropriate template files are created based on the dirs' value

--- commands/create.js
+++ commands/create.js
@@ -1,21 +1,17 @@
 'use strict';
 
 var download = require('../lib/download');
-// var mkdirs = require('mkdirs');
-
+var mkdirs = require('../lib/mkdirs');
 module.exports = {
   "command": "create <manifest> <projectName>",
   "description": "Creates the scaffolding based on the provided manifest file",
   "action": function(manifest, projectName) {
-    var test = download(manifest, function(yo) {
-      var manifestContent = null;
-      console.log(yo);
-      try {
-        manifestContent = JSON.parse(yo);
-      } catch (e) {
-        console.log(e);
+    download(manifest, function(err, yo) {
+      if (err) {
+        return err;
       }
-      require('../lib/mkdirs')(projectName, Object.keys(manifestContent));
+      console.log(yo);
+      mkdirs(projectName || './', yo);
     });
   }
 }
